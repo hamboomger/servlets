672b5001398c9478a5dfdbd77c9bee140b48e48c
ddorokhov <ddorokhov@e-point.pl>
2018-01-29 15:48:09 +0100

    main.css and LoginServlet files added

---
+++ src/main/java/pl/epoint/servlets/servlets/LoginServlet.java
@@ -0,0 +1,43 @@
+package pl.epoint.servlets.servlets;
+
+import lombok.extern.log4j.Log4j2;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static pl.epoint.servlets.util.ServletUtils.getRequiredParam;
+
+@Log4j2
+public class LoginServlet extends HttpServlet {
+
+    private int usersIdCounter;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        boolean success = performLogin(req);
+        if(success) {
+            req.getSession().setAttribute("userId", usersIdCounter++);
+            resp.sendRedirect("/products/");
+        } else {
+            req.setAttribute("error", "Login or password is invalid");
+            resp.sendRedirect("/products/login");
+        }
+    }
+
+    private boolean performLogin(HttpServletRequest httpRequest) {
+        String login = getRequiredParam("login", httpRequest);
+        String password = getRequiredParam("password", httpRequest);
+        log.info("Login: {}, password: {}", login, password);
+
+        return login.equals("jack") && password.equals("sparrow");
+    }
+
+}
